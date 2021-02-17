dd3aa046f6685b9b8d05d71cbe64fd90ac9950ea
unknown <dk23606@gmail.com>
2021-02-17 18:48:33 +0530

    edited

--- dev.sh
+++ dev.sh
@@ -2,4 +2,4 @@ nothing is added and im addind it in stash
 giving an stash
 pushing through ssh
 commiting
-:
+hello
--- din.sh
+++ din.sh
@@ -2,4 +2,5 @@ man this file is edited in dev branch
 hai 
 this is a din file 
 created in an editor master branch 
+editing
 
