fc8a97c43e03dc92c976a89ca31a6e0ca1088249
dineshsar <dk23606@gmail.com>
2021-03-14 23:14:37 +0530

    adev

--- a.sh
+++ a.sh
@@ -1,2 +1,6 @@
 this is sample file
 created this file in development branch and its reflecting in master branch too
+commited this file from master branch and file is not seen in dev branch
+after rebasing this file in dev branch it reflects in dev branch
+added content in this file from dev branch ..with out commit reflects in master branch
+
