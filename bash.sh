87206cb1d1a0835079880ad8f0d1e26dd3ac61d1
dineshsar <dk23606@gmail.com>
2021-03-14 22:44:47 +0530

    shell

--- bash.sh
+++ bash.sh
@@ -1,8 +1,1 @@
-hello word 
-this is dinesh 
-creating a file in git bash
-updated in github
-revert
-using develoment branch
-editing file in dev branch commitng file in same branch but trying to push from master branch
-using fetch command
+echo 'hello world'
