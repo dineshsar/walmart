a677d33e0351b8f21828ae85cdb3444f9fc0c601
dineshsar <78139301+dineshsar@users.noreply.github.com>
2021-03-15 12:10:20 +0530

    Delete a.sh

--- a.sh
+++
@@ -1,6 +0,0 @@
-this is sample file
-created this file in development branch and its reflecting in master branch too
-commited this file from master branch and file is not seen in dev branch
-after rebasing this file in dev branch it reflects in dev branch
-added content in this file from dev branch ..with out commit reflects in master branch
-commited from dev branch and doesnot reflected in master branch
