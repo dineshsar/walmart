b7070a06b75f2dcb780fb5beea8c553c68c8c9cf
anilkumarakr <anilreddy1998@gmail.com>
2021-03-30 19:36:42 +0530

    one

--- anilbash.sh
+++ anilbash.sh
@@ -5,10 +5,10 @@ read username
 echo "Enter password"
 echo "if u enter the wrong password u have an awrd"
 read password
-if [[ ( $username == "admin" && $password == "panati" ) ]]; then
+if  [[ ( $username == "admin" && $password  == "panati" ) ]]; then
 echo "valid user"
 else
-echo "black sheep"
+echo  "black sheep"
 echo "fuck u"
 fi
 echo "bye"
--- c1.sh
+++ c1.sh
@@ -3,3 +3,19 @@ this file is created in feature branch as c1
 commited in c1
 added content in dev 
 rebase
+||||||| parent of 7506711 (one)
+hello
+this file is created in feature branch as c1
+commited in c1
+add anill acount in git
+for((c=1;c<11;c++))
+do
+read -r c
+i=1
+while [ $i -le 1 ]
+do
+b=`expr $c \* $i`
+echo "$c * $i = $b"
+i=`expr $i + 1`
+done
+done
